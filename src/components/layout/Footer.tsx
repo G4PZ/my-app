78e16bd5509ff22e2c1291ae5e6259623833c6de
G4PZ <giancarlospagadorz@gmail.com>
2025-09-15 22:03:25 -0500

    refactor: Simplify structure and improve layout consistency in Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 export default function Footer() {
   return (
-    <div className="bg-black w-full px-2 py-2 ">
-      <div className=" w-full py-[8px] flex items-center px-4 bg-gradient-to-r from-footer via-purple-500 to-footerSky">
-        <div className="flex w-full justify-between max-w-[1920px] ">
-          <section className="text-textBase text-[14px] ">
-            <h2 className="font-bold">Muestra de Spotify</h2>
-            <p className=" font-semibold text-[16px] ">
-              Regístrate para acceder a canciones y podcasts ilimitados con
-              algunos anuncios. No necesitas tarjeta de crédito.
-            </p>
-          </section>
-          <button className=" flex justify-center items-center px-8 py-[9px] rounded-full bg-textBase text-black font-bold hover:bg-gray-200">
-            Regístrate gratis
-          </button>
-        </div>
+    <div className=" flex justify-center items-center max-w-[1920px] bg-black px-2 py-2 w-full">
+      <div className=" flex  justify-between  py-[8px] px-4  bg-gradient-to-r w-full from-footer via-purple-500 to-footerSky">
+        <section className="text-textBase text-[14px] ">
+          <h2 className="font-bold">Muestra de Spotify</h2>
+          <p className=" font-semibold text-[16px] ">
+            Regístrate para acceder a canciones y podcasts ilimitados con
+            algunos anuncios. No necesitas tarjeta de crédito.
+          </p>
+        </section>
+        <button className="  px-8 py-[9px] rounded-full bg-textBase text-black font-bold hover:bg-gray-200">
+          Regístrate gratis
+        </button>
       </div>
     </div>
   );
